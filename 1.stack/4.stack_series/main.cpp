d022528d234bdfc08c25010bda9cd3627dde4fd0
Hojung Shin <shinhj0827@hanyang.ac.kr>
2022-09-09 22:37:04 +0900

    1.stack - 4.stack_series

---
+++ 1.stack/4.stack_series/main.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <string>
+#include <string.h>
+#include <map>
+#include <sstream>
+#include "stack.h"
+
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    freopen("input.txt", "r", stdin);
+
+    int series_cnt = 1, target, prev = -1, N = 1;
+    stack<int> stk{MAX_N};
+    char buffer[20];
+    int buffer_size = 0;
+
+    cin >> series_cnt;
+
+    for (; series_cnt > 0; --series_cnt) {
+        cin >> target;
+    
+        if (prev < target) {
+            int ret = stk.pop();
+            if (ret != prev) {
+                cout << "NO" << endl;
+                return 0;
+            }
+            buffer[buffer_size++] = '-';
+            for (; N <= target; ++N) {
+                stk.push(N);
+                buffer[buffer_size++] = '+';
+            }
+        } else { // prev > target
+            int ret = stk.pop();
+            if (ret != prev) {
+                cout << "NO" << endl;
+                return 0;
+            }
+            buffer[buffer_size++] = '-';
+        }
+        prev = target;
+    }
+
+    int ret = stk.pop();
+    if (ret != prev) {
+        cout << "NO" << endl;
+        return 0;
+    }
+    buffer[buffer_size++] = '-';
+
+    for (size_t i = 1; i < buffer_size; ++i) {
+        printf("%c\n", buffer[i]);
+    }
+    
+    return 0;
+}
